1479c5fa32ff565e8173a3add40aaf027fb341fd
diego passos dos santos <diegopassosdossantos@pop-os.localdomain>
2023-10-05 16:37:11 -0300

    first commit

---
+++ openai_api/client.py
@@ -0,0 +1,15 @@
+import openai
+
+def get_car_ai_bio(model, brand, year):
+    prompt = '''
+    Me mostre uma descrição de venda para o carro {}{}{} em apenas 250 caracteres. Fale coisas específicas desse modelo de carro.
+    '''
+    openai.api_key = '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'
+    prompt = prompt.format(brand, model, year)
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=800,
+    )
+    return response['choices'][0]['text']
+
